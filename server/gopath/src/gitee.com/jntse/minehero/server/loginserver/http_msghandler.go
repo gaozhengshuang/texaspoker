c9d700a4aa1482f8a3d91d99acc29303878c9e37
jntse <xiejian1998@qq.com>
2018-07-13 16:03:58 +0800

    微信小游戏登陆流程修改

--- server/gopath/src/gitee.com/jntse/minehero/server/loginserver/http_msghandler.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/loginserver/http_msghandler.go
@@ -7,7 +7,9 @@ import (
 	_"github.com/go-redis/redis"
 	"gitee.com/jntse/gotoolkit/log"
 	"gitee.com/jntse/gotoolkit/util"
+	"gitee.com/jntse/gotoolkit/net"
 	"gitee.com/jntse/minehero/server/def"
+	"gitee.com/jntse/minehero/server/tbl"
 )
 
 // 定义Http解析参数 字段名首字母必须大写
@@ -29,6 +31,17 @@ type HttpArguRegitstAccount struct {
 	Nickname	string
 }
 
+type HttpArguWxLogin struct {
+	TempAuthCode string	// 微信临时登录凭证code
+}
+
+type HttpRespWxLogin struct {
+	Openid	string	// 用户唯一标识
+	Session_key	string 	//会话密钥
+	Unionid	string	// 用户在开放平台的唯一标识符
+}
+
+
 // TODO：回调函数有http底层单独的协程调用，想要单线程处理可以将数据push到主线程chan中
 func HttpServerResponseCallBack(w http.ResponseWriter, urlpath string, rawquery string, body []byte) {
 	log.Info("HttpServerResponseCallBack[rid=%d]", util.GetRoutineID())
@@ -37,7 +50,7 @@ func HttpServerResponseCallBack(w http.ResponseWriter, urlpath string, rawquery 
 	log.Info("urlpath: %s", urlpath)
 	log.Info("rawquery: %s", rawquery)
 	log.Info("body: %s", string(body))
-	respbody := ""
+	respjson := ""
 
 	switch {
 	default:
@@ -47,16 +60,22 @@ func HttpServerResponseCallBack(w http.ResponseWriter, urlpath string, rawquery 
 		objerror := json.Unmarshal(body, objcmd)
 		if objerror != nil {
 			log.Error("HttpServerResponseCallBack json.Unmarshal to HttpArguBaseCmd err[%s]", objerror)
-			respbody = "无法解析json参数"
+			respjson = PackHttpResponse(1, "无法解析json参数")
 			break
 		}
 
 		// 注册账户
 		if objcmd.GMCmd == "registauthcode" {
-			respbody = HttpGetRegistAuthCode(body)
+			errcode, errmsg := HttpGetRegistAuthCode(body)
+			respjson = PackHttpResponse(errcode, errmsg)
 			break
 		}else if objcmd.GMCmd == "registaccount" {
-			respbody = HttpRegistAccount(body)
+			errcode, errmsg := HttpRegistAccount(body)
+			respjson = PackHttpResponse(errcode, errmsg)
+			break
+		}else if objcmd.GMCmd == "wx_login" {
+			errcode, errmsg := HttpWxLogin(body)
+			respjson = PackHttpResponse(errcode, errmsg)
 			break
 		}
 
@@ -66,14 +85,15 @@ func HttpServerResponseCallBack(w http.ResponseWriter, urlpath string, rawquery 
 		unerr := json.Unmarshal(body, &cmdmap)
 		if unerr != nil {
 			log.Error("HttpServerResponseCallBack json.Unmarshal to map[string]interface{} err[%s]", unerr)
-			respbody = "无法解析json参数"
+			respjson = PackHttpResponse(1, "无法解析json参数")
 			break
 		}
 
 		if _ , ok := cmdmap["gmcmd"]; ok {
 			gmcommands:= make(map[string]string)
 			for k ,v := range cmdmap { gmcommands[k] = v.(string) }
-			respbody = DoGMCmd(gmcommands)
+			errcode, errmsg := DoGMCmd(gmcommands)
+			respjson = PackHttpResponse(errcode, errmsg)
 			break
 		}
 	}
@@ -85,24 +105,22 @@ func HttpServerResponseCallBack(w http.ResponseWriter, urlpath string, rawquery 
 	head.Set("Access-Control-Allow-Origin", "*")       // 允许客户端跨域
 	//head.Set("Content-Type", "application/json")
 	//head.Set("Content-Type", "application/x-www-form-urlencoded")
-
-	// ret code
-	respcode := 0
-	if respbody != "" { respcode = -1 }
-	respjson := fmt.Sprintf(`{"status":%d, "msg":"%s"}`, respcode, respbody)
 	w.WriteHeader(http.StatusOK)
 	//w.WriteHeader(http.StatusNotFound)
 	//w.Write([]byte("hello golang http test"))
 	w.Write([]byte(respjson))
 }
 
+func PackHttpResponse(errcode int32, msg string) string {
+	return fmt.Sprintf(`{"status":%d, "msg":"%s"}`, errcode, msg)
+}
 
 // GM指令处理
-func DoGMCmd(cmd map[string]string) string {
+func DoGMCmd(cmd map[string]string) (errcode int32, errmsg string) {
 	value, ok := cmd["gmcmd"]
 	if !ok {
 		log.Error("找不到gmcmd字段")
-		return "找不到gmcmd字段"
+		return 1, "找不到gmcmd字段"
 	}
 
 	switch value {
@@ -110,60 +128,120 @@ func DoGMCmd(cmd map[string]string) string {
 		return DoReload(cmd)
 	case "wechatpay":
 		return DoWeChatPay(cmd)
+	case "wxbalance":
+		return DoWechatBalance(cmd)
 	}
 
-	return "错误的cmd类型"
+	return 1, "错误的cmd类型"
 }
 
-func DoReload(cmd map[string]string) string {
+func DoReload(cmd map[string]string) (errcode int32, errmsg string) {
 	Login().Reload()
-	return ""
+	return 0 , "Reload OK"
 }
 
-func DoWeChatPay(cmd map[string]string) string {
+func DoWeChatPay(cmd map[string]string) (errcode int32, errmsg string) {
 	openid , amount := cmd["openid"], cmd["amount"]
 	num , _:= strconv.ParseInt(amount, 10, 32)
 	def.HttpWechatCompanyPay(openid, num)
-	return ""
+	return 0, "DoWeChatPay OK"
 }
 
-func HttpGetRegistAuthCode(body []byte) string {
+func DoWechatBalance(cmd map[string]string) (errcode int32, errmsg string) {
+	openid := cmd["openid"]
+	def.HttpWechatMiniGameGetBalance(Redis(), openid)
+	return 0, "DoWechatBalance OK"
+}
+
+
+func HttpGetRegistAuthCode(body []byte) (errcode int32, errmsg string) {
 	objcmd := &HttpArguAccountAuthCode{}
 	objerror := json.Unmarshal(body, objcmd)
 	if objerror != nil {
 		log.Error("json.Unmarshal to HttpArguAccountAuthCode err[%s]", objerror)
-		return "解析json参数失败"
+		return 1, "解析json参数失败"
 	}
 
-	return GetRegistAuthCode(objcmd.Phone)
+	if errmsg := GetRegistAuthCode(objcmd.Phone); errmsg != "" {
+		return 1, errmsg
+	}
+
+	return 0, "成功"
 }
 
-func HttpRegistAccount(body []byte) string {
+func HttpRegistAccount(body []byte) (errcode int32, errmsg string) {
 	objcmd := &HttpArguRegitstAccount{}
 	objerror := json.Unmarshal(body, objcmd)
 	if objerror != nil {
 		log.Error("json.Unmarshal to HttpArguRegitstAccount err[%s]", objerror)
-		return "解除json参数失败"
+		return 1, "解除json参数失败"
 	}
 
-	errcode, phone, passwd, authcode, invitationcode := "", objcmd.Phone, objcmd.Passwd, objcmd.Authcode, objcmd.Invitationcode
+	phone, passwd, authcode, invitationcode := objcmd.Phone, objcmd.Passwd, objcmd.Authcode, objcmd.Invitationcode
 	nickname, account := objcmd.Nickname, phone
 
 	switch {
 	default:
-		if errcode = RegistAccountCheck(account, passwd, invitationcode, authcode, nickname); errcode != "" {
+		if errmsg = RegistAccountCheck(account, passwd, invitationcode, authcode, nickname); errmsg != "" {
 			break
 		}
 
 		// 验证通过
-		if errcode = RegistAccount(account, passwd, invitationcode, nickname, "", ""); errcode != "" {
+		if errmsg = RegistAccount(account, passwd, invitationcode, nickname, "", ""); errmsg != "" {
 			break
 		}
 	}
 
 	// 回复客户端
-	if errcode != "" { log.Info("[注册] 账户[%s] 注册失败[%s]", phone, errcode) }
-	return errcode
+	if errmsg != "" { 
+		log.Info("[注册] 账户[%s] 注册失败[%s]", phone, errmsg)
+		return 1, errmsg
+	}
+	return 0, "成功"
+}
+
+
+func HttpWxLogin(body []byte) (errcode int32, errmsg string) {
+	objcmd := &HttpArguWxLogin{}
+	objerror := json.Unmarshal(body, objcmd)
+	if objerror != nil {
+		log.Error("json.Unmarshal to HttpArguRegitstAccount err[%s]", objerror)
+		return 1, "解除json参数失败"
+	}
+
+	if objcmd.TempAuthCode == "" {
+		return 1, "临时登录凭证code是空"
+	}
+
+	//
+	url := fmt.Sprintf(tbl.Global.WechatMiniGame.Jscode2Session, tbl.Global.Wechat.AppID, tbl.Global.Wechat.AppSecret, objcmd.TempAuthCode)
+	resp, err := network.HttpGet(url)
+	if err != nil {
+		log.Error("HttpWxLogin http请求错误 err[%s]", err)
+		return 1, "http请求错误"
+	}
+
+	objResp := &HttpRespWxLogin{}
+	unerr := json.Unmarshal(resp.Body, objResp)
+	if unerr != nil {
+		log.Error("HttpWxLogin Json Unmarshal失败 err[%s]", unerr)
+		return 1, "Json Unmarshal失败"
+	}
+
+	if objResp.Openid == ""  || objResp.Session_key == "" {
+		log.Error("HttpWxLogin 获取OpenId失败 resp[%s]", resp.Body)
+		return 1, "获取OpenId失败"
+	}
+
+	// 绑定Openid和Session_key
+	setkey := fmt.Sprintf("wechat_openid_%s_sessionkey", objResp.Openid)
+	_, seterr := Redis().Set(setkey, objResp.Session_key, 0).Result()
+	if seterr != nil {
+		return 1, "绑定Openid和Session_key失败"
+	}
+
+	reply := fmt.Sprintf("%s", objResp.Openid)
+	return 0, reply
 }
 
 
