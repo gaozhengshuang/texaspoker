eddc1dea56b23b17733a4dc38b14733d440958d7
jntse <xiejian1998@qq.com>
2018-07-15 00:19:37 +0800

    golang使用静态库修改

--- server/gopath/src/gitee.com/jntse/testgo/testuselib/hello.go
+++ server/gopath/src/gitee.com/jntse/testgo/testuselib/hello.go
@@ -4,15 +4,15 @@ import (     // import packages
 	"fmt"
 )
 
-import "testgolib"
+import "testbuildlib"
 
 
 func main() {
 	str := "字符串翻转测试"
 	fmt.Println("翻转前:", str)
-	fmt.Println("翻转后:", makegolib.Reverse(str))
+	fmt.Println("翻转后:", libjntse.Reverse(str))
 
 	str = "123456789"
 	fmt.Println("翻转前:", str)
-	fmt.Println("翻转后:", makegolib.Reverse(str))
+	fmt.Println("翻转后:", libjntse.Reverse(str))
 }
