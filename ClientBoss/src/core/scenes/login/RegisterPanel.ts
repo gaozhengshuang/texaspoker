9233a3390c21dfe134c6eed49cac834e454602a3
hongdawei <84318436@qq.com>
2018-07-17 11:18:24 +0800

    强迫症提交

--- ClientBoss/src/core/scenes/login/RegisterPanel.ts
+++ ClientBoss/src/core/scenes/login/RegisterPanel.ts
@@ -16,8 +16,10 @@ module game {
             return RegisterPanelSkin;
         }
 
-        protected init() {            this.registerButton.icon = "login_json.registerBtn";        this.btn_authCode.icon = "login_json.yzmbutton";
-    }
+        protected init() {
+            this.registerButton.icon = "login_json.registerBtn";
+            this.btn_authCode.icon = "login_json.yzmbutton";
+        }
 
         protected beforeShow() {
             this._touchEvent = [
