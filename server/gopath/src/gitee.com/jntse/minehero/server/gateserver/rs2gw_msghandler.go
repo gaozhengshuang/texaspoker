f52d7ead91508ade896449b58e340c1d87163512
jntse <xiejian1998@qq.com>
2018-07-19 15:28:47 +0800

    充值bug

--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/rs2gw_msghandler.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/rs2gw_msghandler.go
@@ -53,6 +53,7 @@ func (this* RS2GWMsgHandler) Init() {
 	this.msgparser.RegistSendProto(msg.BT_ReqQuitGameRoom{})
 	this.msgparser.RegistSendProto(msg.BT_UpdateMoney{})
 	this.msgparser.RegistSendProto(msg.C2GW_StartLuckyDraw{})
+	this.msgparser.RegistSendProto(msg.C2GW_PlatformRechargeDone{})
 	this.msgparser.RegistSendProto(msg.BT_ReqLaunchBullet{})
 	this.msgparser.RegistSendProto(msg.BT_StepOnBomb{})
 	this.msgparser.RegistSendProto(msg.BT_BulletEarnMoney{})
