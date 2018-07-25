2858088a713ffb99d884c28f139c0342124be264
jntse <xiejian1998@qq.com>
2018-07-25 15:50:08 +0800

    换装系统

--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/userimage.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/userimage.go
@@ -8,14 +8,18 @@ import (
 /// @brief 换装形象
 // --------------------------------------------------------------------------
 type UserImage struct {
-	female 	map[uint32]*msg.ItemData
-	male	map[uint32]*msg.ItemData
-	owner   *GateUser
+	female 	map[int32]*msg.ItemData
+	male		map[int32]*msg.ItemData
+	//clothes 	map[int32]map[int32]*msg.ItemData
+	owner   	*GateUser
 }
 
 func (this *UserImage) Init(user *GateUser) {
-	this.male = make(map[uint32]*msg.ItemData)
-	this.female = make(map[uint32]*msg.ItemData)
+	this.male = make(map[int32]*msg.ItemData)
+	this.female = make(map[int32]*msg.ItemData)
+	//this.clothes = make(map[int32]map[int32]*msg.ItemData)
+	//this.clothes[int32(msg.Sex_Female] = make(map[int32]*msg.ItemData)
+	//this.clothes[int32(msg.Sex_Male] = make(map[int32]*msg.ItemData)
 	this.owner = user
 }
 
@@ -23,9 +27,9 @@ func (this *UserImage) LoadBin(bin *msg.Serialize) {
 	for _, image := range bin.GetBase().Images.Lists {
 		for _, item := range image.Clothes {
 			if image.GetSex() == int32(msg.Sex_Female) {
-				this.female[item.GetId()] = item
+				this.female[item.GetPos()] = item
 			}else if image.GetSex() == int32(msg.Sex_Male) {
-				this.male[item.GetId()] = item
+				this.male[item.GetPos()] = item
 			}
 		}
 	}
@@ -49,9 +53,91 @@ func (this *UserImage) PackBin(bin *msg.Serialize) {
 }
 
 func (this *UserImage) Clean() {
-	this.male = make(map[uint32]*msg.ItemData)
-	this.female = make(map[uint32]*msg.ItemData)
+	this.male = make(map[int32]*msg.ItemData)
+	this.female = make(map[int32]*msg.ItemData)
 }
 
-func (this *UserImage) DressClothes() {
+func (this *UserImage) GetClothesByPos(pos int32) *msg.ItemData {
+	if this.owner.Sex() == int32(msg.Sex_Female) {
+		if item, find := this.female[pos]; find == true { return item }
+	}else if this.owner.Sex() == int32(msg.Sex_Female) {
+		if item, find := this.male[pos]; find == true { return item }
+	}
+	return nil
 }
+
+func (this *UserImage) SendShowImage() {
+	send := &msg.GW2C_SendShowImage{Images:&msg.ImageData{Sex:pb.Int32(this.owner.Sex()), Clothes:make([]*msg.ItemData,0)} }
+	if this.owner.Sex() == int32(msg.Sex_Female) {
+		for _, item := range this.female { 
+			send.Images.Clothes = append(send.Images.Clothes, item) 
+		}
+	}else if this.owner.Sex() == int32(msg.Sex_Male) {
+		for _, item := range this.male { 
+			send.Images.Clothes = append(send.Images.Clothes, item) 
+		}
+	}
+
+	this.owner.SendMsg(send)
+}
+
+func (this *UserImage) DressClothes(pos int32, itemid int32) {
+	newEquip := this.owner.bag.FindById(uint32(itemid))
+	if newEquip == nil {
+		this.owner.SendNotify("找不到穿戴的服装")
+		return
+	}
+
+	equipbase := newEquip.EquipBase()
+	if equipbase == nil {
+		this.owner.SendNotify("只能穿戴服装道具")
+		return 
+	}
+
+	if equipbase.Pos != pos {
+		this.owner.SendNotify("不能穿戴这个位置")
+		return
+	}
+
+	// 
+	copyItem := pb.Clone(newEquip.Bin()).(*msg.ItemData)
+	copyItem.Pos = pb.Int32(pos)
+	
+
+	if this.owner.Sex() == int32(msg.Sex_Female) {
+		this.female[pos] = copyItem
+	}else if this.owner.Sex() == int32(msg.Sex_Male) {
+		this.male[pos] = copyItem
+	}
+
+	this.SendShowImage()
+	//send := &msg.GW2C_UpdateItemPos{Items:make([]*msg.ItemData,0)}
+	//send.Items = append(send.Items, pb.Clone(newEquip.Bin()).(*msg.ItemData))
+	//this.owner.SendMsg(send)
+}
+
+// 脱下服装
+func (this *UserImage) UnDressClothes(pos int32, syn bool) {
+	if clothes := this.GetClothesByPos(pos); clothes == nil {
+		this.owner.SendNotify("没有需要脱下的服装")
+		return
+	}
+
+	if this.owner.Sex() == int32(msg.Sex_Female) {
+		delete(this.female, pos)
+	}else if this.owner.Sex() == int32(msg.Sex_Male) {
+		delete(this.male, pos)
+	}
+
+	if syn { this.SendShowImage() }
+}
+
+func (this *UserImage) UnDressAll(syn bool) {
+	if this.owner.Sex() == int32(msg.Sex_Female) {
+		this.female = make(map[int32]*msg.ItemData)
+	}else if this.owner.Sex() == int32(msg.Sex_Male) {
+		this.male = make(map[int32]*msg.ItemData)
+	}
+	if syn { this.SendShowImage() }
+}
+
