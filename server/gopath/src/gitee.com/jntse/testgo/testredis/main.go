441590d2009a0f9c02cb97c36848627deaa9fe64
jntse <xiejian1998@qq.com>
2018-08-10 17:24:51 +0800

    redis cluster 测试用例

--- server/gopath/src/gitee.com/jntse/testgo/testredis/main.go
+++ server/gopath/src/gitee.com/jntse/testgo/testredis/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"github.com/go-redis/redis"
 	pb "github.com/golang/protobuf/proto"
 	"gitee.com/jntse/testgo/testredis/proto"
@@ -32,7 +33,10 @@ func main() {
 	//TestBin()
 
 	// pipeline
-	TestPipeline()
+	//TestPipeline()
+
+	// cluster
+	TestRedisCluster()
 
 }
 
@@ -52,7 +56,7 @@ func ExampleNewClient() *redis.Client {
 
 	GRedis = client
 	pong, err := client.Ping().Result()
-	fmt.Println(pong, err)
+	log.Println(pong, err)
 	// Output: PONG <nil>
 	return GRedis
 }
@@ -63,27 +67,27 @@ func TestKeys() {
 	if ret1, err1 := GRedis.Set(key1, "user1", 0).Result(); err1 != nil {
 		panic(err1)
 	}else {
-		fmt.Printf("set key=%s status[%s] \n", key1 , ret1)
+		log.Printf("set key=%s status[%s] \n", key1 , ret1)
 	}
 
 	if val1, err2 := GRedis.Get(key1).Result(); err2 == redis.Nil {
-		fmt.Println("key not found:" , key1)
+		log.Println("key not found:" , key1)
 	}else if( err2 != nil ) {
-		fmt.Println("TestKeys err2:", err2)
+		log.Println("TestKeys err2:", err2)
 	}else {
-		fmt.Printf("%s=%s\n", key1, val1)
+		log.Printf("%s=%s\n", key1, val1)
 	}
 
 
 	if _, err3 := GRedis.Get("goredis_keys").Result(); err3 == redis.Nil {
-		fmt.Println("key username not found")
+		log.Println("key username not found")
 	}
 
 
 	val2, _:= GRedis.Exists(key1).Result()
 	val3, _:= GRedis.Exists("goredis_keys_1").Result()
-	fmt.Printf("key %s exists[%d]\n", key1, val2)
-	fmt.Printf("key %s exists[%d]\n", "goredis_keys_1", val3)
+	log.Printf("key %s exists[%d]\n", key1, val2)
+	log.Printf("key %s exists[%d]\n", "goredis_keys_1", val3)
 }
 
 func TestIncr() {
@@ -95,14 +99,14 @@ func TestIncr() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("incr %s ret %v\n", key, ret)
+	log.Printf("incr %s ret %v\n", key, ret)
 
 	// key存在
 	ret1, err1 := GRedis.Incr(key).Result()
 	if err1 != nil {
 		panic(err1)
 	}
-	fmt.Printf("incr %s ret %v\n", key, ret1)
+	log.Printf("incr %s ret %v\n", key, ret1)
 }
 
 
@@ -121,15 +125,15 @@ func TestList() {
 	// 一次多个
 	err2 := GRedis.LPush(key, 101, 102).Err()
 	if err2 != nil {
-		fmt.Println("TestList err2:", err2)
+		log.Println("TestList err2:", err2)
 	}
 
 	//获取
 	rlist, err3 := GRedis.LRange(key, 0, 5).Result()
 	if err3 != nil {
-		fmt.Println("TestList err3:", err3)
+		log.Println("TestList err3:", err3)
 	}
-	fmt.Printf("TestList rlist=%#v\n", rlist)
+	log.Printf("TestList rlist=%#v\n", rlist)
 }
 
 
@@ -142,7 +146,7 @@ func TestSet() {
 	}
 
 	len := GRedis.SCard(key).Val()
-	fmt.Printf("TestSet key[%s] len[%d]\n", key, len)
+	log.Printf("TestSet key[%s] len[%d]\n", key, len)
 
 	// 添加
 	if err := GRedis.SAdd(key, 10).Err(); err != nil {
@@ -151,36 +155,36 @@ func TestSet() {
 
 	// 一次多个
 	if err := GRedis.SAdd(key, 11, 12).Err(); err != nil {
-		fmt.Println("TestSet err2:", err)
+		log.Println("TestSet err2:", err)
 	}
 
 	// 重复添加
 	if val , err := GRedis.SAdd(key, 10).Result(); val == 0 || err != nil {
 		if err != nil { 
-			fmt.Println("TestSet duplicate key 10, err: ", err) 
+			log.Println("TestSet duplicate key 10, err: ", err) 
 		} else { 
-			fmt.Println("TestSet key 10 is duplicate") 
+			log.Println("TestSet key 10 is duplicate") 
 		}
 	}
 
 	//获取
 	rlist, err3 := GRedis.SMembers(key).Result()
 	if err3 != nil {
-		fmt.Println("TestSet err3:", err3)
+		log.Println("TestSet err3:", err3)
 	}
-	fmt.Printf("TestSet rlist=%#v\n", rlist)
+	log.Printf("TestSet rlist=%#v\n", rlist)
 
 
 	// 查找
 	isfind, err4 := GRedis.SIsMember(key, 12).Result()
 	if err4 != nil {
-		fmt.Println("TestSet err4:", err4)
+		log.Println("TestSet err4:", err4)
 	} else if isfind == true {
-		fmt.Println("TestSet key 12 is member")
+		log.Println("TestSet key 12 is member")
 	}
 
 	len = GRedis.SCard(key).Val()
-	fmt.Printf("TestSet key[%s] len[%d]\n", key, len)
+	log.Printf("TestSet key[%s] len[%d]\n", key, len)
 }
 
 
@@ -189,13 +193,13 @@ func TestZSet() {
 	// Size
 	key := fmt.Sprintf("goredis_zset_%d", 1001)
 	len := GRedis.ZCard(key).Val()
-	fmt.Printf("key %s len %d\n", key, len)
+	log.Printf("key %s len %d\n", key, len)
 
 
 	// 添加
 	err := GRedis.ZAdd(key, redis.Z{8, "fruit"}).Err()
 	if err == redis.Nil {
-		fmt.Printf("TestZSet ZAdd fruit fail")
+		log.Printf("TestZSet ZAdd fruit fail")
 	}else if err != nil {
 		panic(err)
 	}
@@ -204,43 +208,43 @@ func TestZSet() {
 	// 一次多个
 	err2 := GRedis.ZAdd(key, redis.Z{15, "meat"} , redis.Z{3, "vegetables"}).Err()
 	if err2 != nil {
-		fmt.Println("TestZSet err2:", err2)
+		log.Println("TestZSet err2:", err2)
 	}
 
 
 	// 获取排名, 0开始
 	rank, err3 := GRedis.ZRank(key, "fruit").Result()
 	if err3 != nil {
-		fmt.Println("TestZSet err3:", err3)
+		log.Println("TestZSet err3:", err3)
 	}
-	fmt.Printf("TestZSet fruit rank=%d\n", rank)
+	log.Printf("TestZSet fruit rank=%d\n", rank)
 
 
 	// 获取前10排名列表
 	rlist, err4 := GRedis.ZRange(key, 0, 10).Result()
 	if err4 != nil {
-		fmt.Println("TestZSet err4:", err4)
+		log.Println("TestZSet err4:", err4)
 	}
-	fmt.Printf("TestZSet range:%v\n", rlist)
+	log.Printf("TestZSet range:%v\n", rlist)
 
 
 	// 获取带分数的排名
 	wscores0, err5  := GRedis.ZRangeWithScores(key, 0, 5).Result()
 	wscores1, errrev := GRedis.ZRevRangeWithScores(key, 0, 5).Result()
 	if err5 != nil || errrev != nil {
-		fmt.Println("TestZSet err5:",  err5)
-		fmt.Println("TestZSet err52:", errrev)
+		log.Println("TestZSet err5:",  err5)
+		log.Println("TestZSet err52:", errrev)
 	}
-	fmt.Printf("TestZSet rank with scores=%v\n", wscores0)
-	fmt.Printf("TestZSet revrank with scores =%v\n", wscores1)
+	log.Printf("TestZSet rank with scores=%v\n", wscores0)
+	log.Printf("TestZSet revrank with scores =%v\n", wscores1)
 
 
 	// 获取在[min,max]分数区间内的列表
 	rscores, err6 := GRedis.ZRangeByScore(key, redis.ZRangeBy{"0", "15", 0, 0}).Result()
 	if err6 != nil {
-		fmt.Println("TestZSet err6:", err5)
+		log.Println("TestZSet err6:", err5)
 	}
-	fmt.Printf("TestZSet range:%v\n", rscores)
+	log.Printf("TestZSet range:%v\n", rscores)
 
 }
 
@@ -253,13 +257,13 @@ func SetBin(msg pb.Message) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Printf("TestBin Marshal len=%d\n", len(buf))
+	log.Printf("TestBin Marshal len=%d\n", len(buf))
 
 	// Set二进制
 	key := fmt.Sprintf("goredis_bin_%d", 1001)
 	err2 := GRedis.Set(key, buf, 0).Err()
 	if err2 != nil {
-		fmt.Printf("TestBin err2=%v\n", err2)
+		log.Printf("TestBin err2=%v\n", err2)
 		return
 	}
 }
@@ -270,10 +274,10 @@ func GetBin(msg pb.Message) {
 	key := fmt.Sprintf("goredis_bin_%d", 1001)
 	rstr, err3 := GRedis.Get(key).Result()
 	if err3 != nil {
-		fmt.Printf("TestBin err3=%v\n", err3)
+		log.Printf("TestBin err3=%v\n", err3)
 		return
 	}
-	fmt.Printf("TestBin getbin len=%d\n", len(rstr))
+	log.Printf("TestBin getbin len=%d\n", len(rstr))
 
 
 	// 反序列化
@@ -295,12 +299,12 @@ func TestBin() {
 		Level : pb.Int(1),
 		Exp : pb.Int(0),
 	}
-	fmt.Printf("TestBin Marshal msg=%+v\n", msg)
+	log.Printf("TestBin Marshal msg=%+v\n", msg)
 	SetBin(msg)
 
 	rmsg := &user.UserData{}
 	GetBin(rmsg)
-	fmt.Printf("TestBin Unmarshal rmsg=%+v\n", rmsg)
+	log.Printf("TestBin Unmarshal rmsg=%+v\n", rmsg)
 
 }
 
@@ -312,19 +316,75 @@ func TestPipeline() {
 	// 普通方式
 	for i:=0; i < count; i++ {
 		_, err := GRedis.Get(fmt.Sprintf("%s_%d",key1,i)).Result()
-		if err != nil { fmt.Println("Redis Get Error: ", err) }
+		if err != nil { log.Println("Redis Get Error: ", err) }
 	}
 	
 	// 管道方式
 	pipe := GRedis.Pipeline()
 	for i:=0; i < count; i++ {
 		_, err := pipe.Get(fmt.Sprintf("%s_%d",key2,i)).Result()
-		if err != nil { fmt.Println("Redis Get Error: ", err) }
+		if err != nil { log.Println("Redis Get Error: ", err) }
 	}
 	cmds, err := pipe.Exec()
 	if err != nil {
-		fmt.Println("Pipe Exec Error: ", err)
-		for _, v := range cmds { fmt.Println(v) }
+		log.Println("Pipe Exec Error: ", err)
+		for _, v := range cmds { log.Println(v) }
 	}
 }
+
+
+
+// Cluster
+func TestRedisCluster() {
+
+	// cluster nodes
+	nodes := make([]string, 0)
+	for i:=6274; i <= 6279; i++ {
+		nodes = append(nodes, fmt.Sprintf("127.0.0.1:%d", i))
+	}
+
+	//ClusterSlots :=  func() (slots []redis.ClusterSlot, err error) {
+	//	for _, slot := range slots { log.Println("slot: ", slot) }
+	//	return slots, err
+	//}
+
+	// client
+	var client *redis.ClusterClient
+	client = redis.NewClusterClient(&redis.ClusterOptions{ Addrs: nodes} )
+	err := client.Ping().Err()
+	if err != nil {
+		log.Println("redis cluster ping err: ", err)
+		return
+	}
+
+	// flush all
+	//client.FlushAll().Result()
+
+
+	// strings
+	client.Set("cluster", "hello", 0).Result()
+	val, err := client.Get("cluster").Result()
+	log.Println("cluster val: ", val)
+
+
+	// set 
+	client.SAdd("addrs", "china shanghai pudong").Result()
+	mem, err := client.SMembers("addrs").Result()
+	log.Println("addrs members: ", mem)
+
+
+	// hset
+	client.HSet("charbase_1001", "uid",  "37").Result()
+	client.HSet("charbase_1001", "name", "jacky").Result()
+	client.HSet("charbase_1001", "old",  "30").Result()
+	client.HSet("charbase_1001", "sex",  "1").Result()
+	client.HSet("charbase_1001", "face", "").Result()
+
+	// sortset
+	client.ZAdd("rankscore", redis.Z{150, "jakcy"})
+	client.ZAdd("rankscore", redis.Z{130, "Jimmy"})
+	client.ZAdd("rankscore", redis.Z{120, "John"})
+
+}
+
 
