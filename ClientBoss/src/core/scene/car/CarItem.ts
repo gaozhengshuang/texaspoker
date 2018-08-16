d70a98ffc1c5799f39ad41fbc387c5de0a513406
hongdawei <84318436@qq.com>
2018-08-17 00:41:30 +0800

    颜色修改

--- ClientBoss/src/core/scene/car/CarItem.ts
+++ ClientBoss/src/core/scene/car/CarItem.ts
@@ -71,10 +71,7 @@ module game {
             //价格
             //this.img_gold.visible  =  true;
             //this.img_diamond.visible = false;
-            this.txt_info.textFlow = [
-                { text:"产能："+ this.itemData.Price, style: { bold: true } },
-                { text:"\n"+"价值："+ this.itemData.Price+"金币", style: { bold: true} },
-            ]
+            this.txt_info.text = "产能："+ this.itemData.RewardPerH + "金币/小时" + "\n"+"价值："+ this.itemData.Price+"金币";
 
             //状态
             //this.stateTxt.text = this.data.parkingid == 0 ? "空闲" : "出征";
