95fdaec7727725bac24a0a81e213bac25fdd85ad
jntse <xiejian1998@qq.com>
2018-08-28 17:43:12 +0800

    女仆管理

---
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/maid.go
@@ -0,0 +1,20 @@
+/// @file maid.go
+/// @brief 女仆相关
+/// @author jackytse, xiejian1998@foxmail.com
+/// @version 1.0
+/// @date 2018-08-28
+package main
+
+type MaidManager struct {
+}
+
+func (ma *MaidManager) LoadBin() {
+}
+
+func (ma *MaidManager) SaveBin() {
+}
+
+func (ma *MaidManager) SaveBin() {
+}
+
+
