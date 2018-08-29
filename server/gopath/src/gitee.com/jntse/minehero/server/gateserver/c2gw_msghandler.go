1590b16064c4a15200d3f3d003e334d2676317b5
Franky <liushuaigeq@163.com>
2018-08-29 20:47:58 +0800

    fix

--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/c2gw_msghandler.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/c2gw_msghandler.go
@@ -182,7 +182,7 @@ func (this *C2GWMsgHandler) Init() {
 	this.msgparser.RegistSendProto(msg.GW2C_ParkCarResult{})
 	this.msgparser.RegistSendProto(msg.GW2C_TakeBackCarResult{})
 	this.msgparser.RegistSendProto(msg.GW2C_TicketCarResult{})
-	this.msgparser.RegistSendProto(msg.GW2C_SynParkingRecord{})11
+	this.msgparser.RegistSendProto(msg.GW2C_SynParkingRecord{})
 	this.msgparser.RegistSendProto(msg.GW2C_TakeBackCarResult{})
 	this.msgparser.RegistSendProto(msg.GW2C_RetTakeCarAutoBackReward{})
 	this.msgparser.RegistSendProto(msg.GW2C_CarAutoBack{})
