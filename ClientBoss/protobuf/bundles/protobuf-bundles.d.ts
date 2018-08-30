b3c0fceb0a597b69d247aabf7c30964efd9e5458
sunboy008 <sunboy_008@163.com>
2018-08-30 16:06:39 +0800

    666

--- ClientBoss/protobuf/bundles/protobuf-bundles.d.ts
+++ ClientBoss/protobuf/bundles/protobuf-bundles.d.ts
@@ -1,4 +1,5 @@
 type Long = protobuf.Long;
+// DO NOT EDIT! This is a generated file. Edit the JSDoc in src/*.js instead and run 'npm run types'.
 
 /** Namespace msg. */
 declare namespace msg {
@@ -4367,6 +4368,9 @@ declare namespace msg {
 
         /** HouseData sumvalue */
         sumvalue?: (number|null);
+
+        /** HouseData tradeuid */
+        tradeuid?: (number|Long|null);
     }
 
     /** Represents an HouseData. */
@@ -4425,6 +4429,9 @@ declare namespace msg {
 
         /** HouseData sumvalue. */
         public sumvalue: number;
+
+        /** HouseData tradeuid. */
+        public tradeuid: (number|Long);
 
         /**
          * Creates a new HouseData instance using the specified properties.
@@ -7066,6 +7073,108 @@ declare namespace msg {
 
         /**
          * Converts this GW2C_AckHouseData to JSON.
+         * @returns JSON object
+         */
+        public toJSON(): { [k: string]: any };
+    }
+
+    /** Properties of a GW2C_UpdateHouseDataOne. */
+    interface IGW2C_UpdateHouseDataOne {
+
+        /** GW2C_UpdateHouseDataOne houseuid */
+        houseuid?: (number|Long|null);
+
+        /** GW2C_UpdateHouseDataOne data */
+        data?: (msg.IHouseData|null);
+
+        /** GW2C_UpdateHouseDataOne isdel */
+        isdel?: (boolean|null);
+    }
+
+    /** Represents a GW2C_UpdateHouseDataOne. */
+    class GW2C_UpdateHouseDataOne implements IGW2C_UpdateHouseDataOne {
+
+        /**
+         * Constructs a new GW2C_UpdateHouseDataOne.
+         * @param [properties] Properties to set
+         */
+        constructor(properties?: msg.IGW2C_UpdateHouseDataOne);
+
+        /** GW2C_UpdateHouseDataOne houseuid. */
+        public houseuid: (number|Long);
+
+        /** GW2C_UpdateHouseDataOne data. */
+        public data?: (msg.IHouseData|null);
+
+        /** GW2C_UpdateHouseDataOne isdel. */
+        public isdel: boolean;
+
+        /**
+         * Creates a new GW2C_UpdateHouseDataOne instance using the specified properties.
+         * @param [properties] Properties to set
+         * @returns GW2C_UpdateHouseDataOne instance
+         */
+        public static create(properties?: msg.IGW2C_UpdateHouseDataOne): msg.GW2C_UpdateHouseDataOne;
+
+        /**
+         * Encodes the specified GW2C_UpdateHouseDataOne message. Does not implicitly {@link msg.GW2C_UpdateHouseDataOne.verify|verify} messages.
+         * @param message GW2C_UpdateHouseDataOne message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encode(message: msg.IGW2C_UpdateHouseDataOne, writer?: protobuf.Writer): protobuf.Writer;
+
+        /**
+         * Encodes the specified GW2C_UpdateHouseDataOne message, length delimited. Does not implicitly {@link msg.GW2C_UpdateHouseDataOne.verify|verify} messages.
+         * @param message GW2C_UpdateHouseDataOne message or plain object to encode
+         * @param [writer] Writer to encode to
+         * @returns Writer
+         */
+        public static encodeDelimited(message: msg.IGW2C_UpdateHouseDataOne, writer?: protobuf.Writer): protobuf.Writer;
+
+        /**
+         * Decodes a GW2C_UpdateHouseDataOne message from the specified reader or buffer.
+         * @param reader Reader or buffer to decode from
+         * @param [length] Message length if known beforehand
+         * @returns GW2C_UpdateHouseDataOne
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decode(reader: (protobuf.Reader|Uint8Array), length?: number): msg.GW2C_UpdateHouseDataOne;
+
+        /**
+         * Decodes a GW2C_UpdateHouseDataOne message from the specified reader or buffer, length delimited.
+         * @param reader Reader or buffer to decode from
+         * @returns GW2C_UpdateHouseDataOne
+         * @throws {Error} If the payload is not a reader or valid buffer
+         * @throws {protobuf.util.ProtocolError} If required fields are missing
+         */
+        public static decodeDelimited(reader: (protobuf.Reader|Uint8Array)): msg.GW2C_UpdateHouseDataOne;
+
+        /**
+         * Verifies a GW2C_UpdateHouseDataOne message.
+         * @param message Plain object to verify
+         * @returns `null` if valid, otherwise the reason why it is not
+         */
+        public static verify(message: { [k: string]: any }): (string|null);
+
+        /**
+         * Creates a GW2C_UpdateHouseDataOne message from a plain object. Also converts values to their respective internal types.
+         * @param object Plain object
+         * @returns GW2C_UpdateHouseDataOne
+         */
+        public static fromObject(object: { [k: string]: any }): msg.GW2C_UpdateHouseDataOne;
+
+        /**
+         * Creates a plain object from a GW2C_UpdateHouseDataOne message. Also converts values to other types if specified.
+         * @param message GW2C_UpdateHouseDataOne
+         * @param [options] Conversion options
+         * @returns Plain object
+         */
+        public static toObject(message: msg.GW2C_UpdateHouseDataOne, options?: protobuf.IConversionOptions): { [k: string]: any };
+
+        /**
+         * Converts this GW2C_UpdateHouseDataOne to JSON.
          * @returns JSON object
          */
         public toJSON(): { [k: string]: any };
@@ -13455,91 +13564,91 @@ declare namespace msg {
         public toJSON(): { [k: string]: any };
     }
 
-    /** Properties of a C2GW_EnableMaidDropTo. */
-    interface IC2GW_EnableMaidDropTo {
+    /** Properties of a GW2C_EnableMaidDropTo. */
+    interface IGW2C_EnableMaidDropTo {
 
-        /** C2GW_EnableMaidDropTo datas */
-        datas?: (msg.IHouseData[]|null);
+        /** GW2C_EnableMaidDropTo houses */
+        houses?: (msg.IHouseData[]|null);
     }
 
-    /** Represents a C2GW_EnableMaidDropTo. */
-    class C2GW_EnableMaidDropTo implements IC2GW_EnableMaidDropTo {
+    /** Represents a GW2C_EnableMaidDropTo. */
+    class GW2C_EnableMaidDropTo implements IGW2C_EnableMaidDropTo {
 
         /**
-         * Constructs a new C2GW_EnableMaidDropTo.
+         * Constructs a new GW2C_EnableMaidDropTo.
          * @param [properties] Properties to set
          */
-        constructor(properties?: msg.IC2GW_EnableMaidDropTo);
+        constructor(properties?: msg.IGW2C_EnableMaidDropTo);
 
-        /** C2GW_EnableMaidDropTo datas. */
-        public datas: msg.IHouseData[];
+        /** GW2C_EnableMaidDropTo houses. */
+        public houses: msg.IHouseData[];
 
         /**
-         * Creates a new C2GW_EnableMaidDropTo instance using the specified properties.
+         * Creates a new GW2C_EnableMaidDropTo instance using the specified properties.
          * @param [properties] Properties to set
-         * @returns C2GW_EnableMaidDropTo instance
+         * @returns GW2C_EnableMaidDropTo instance
          */
-        public static create(properties?: msg.IC2GW_EnableMaidDropTo): msg.C2GW_EnableMaidDropTo;
+        public static create(properties?: msg.IGW2C_EnableMaidDropTo): msg.GW2C_EnableMaidDropTo;
 
         /**
-         * Encodes the specified C2GW_EnableMaidDropTo message. Does not implicitly {@link msg.C2GW_EnableMaidDropTo.verify|verify} messages.
-         * @param message C2GW_EnableMaidDropTo message or plain object to encode
+         * Encodes the specified GW2C_EnableMaidDropTo message. Does not implicitly {@link msg.GW2C_EnableMaidDropTo.verify|verify} messages.
+         * @param message GW2C_EnableMaidDropTo message or plain object to encode
          * @param [writer] Writer to encode to
          * @returns Writer
          */
-        public static encode(message: msg.IC2GW_EnableMaidDropTo, writer?: protobuf.Writer): protobuf.Writer;
+        public static encode(message: msg.IGW2C_EnableMaidDropTo, writer?: protobuf.Writer): protobuf.Writer;
 
         /**
-         * Encodes the specified C2GW_EnableMaidDropTo message, length delimited. Does not implicitly {@link msg.C2GW_EnableMaidDropTo.verify|verify} messages.
-         * @param message C2GW_EnableMaidDropTo message or plain object to encode
+         * Encodes the specified GW2C_EnableMaidDropTo message, length delimited. Does not implicitly {@link msg.GW2C_EnableMaidDropTo.verify|verify} messages.
+         * @param message GW2C_EnableMaidDropTo message or plain object to encode
          * @param [writer] Writer to encode to
          * @returns Writer
          */
-        public static encodeDelimited(message: msg.IC2GW_EnableMaidDropTo, writer?: protobuf.Writer): protobuf.Writer;
+        public static encodeDelimited(message: msg.IGW2C_EnableMaidDropTo, writer?: protobuf.Writer): protobuf.Writer;
 
         /**
-         * Decodes a C2GW_EnableMaidDropTo message from the specified reader or buffer.
+         * Decodes a GW2C_EnableMaidDropTo message from the specified reader or buffer.
          * @param reader Reader or buffer to decode from
          * @param [length] Message length if known beforehand
-         * @returns C2GW_EnableMaidDropTo
+         * @returns GW2C_EnableMaidDropTo
          * @throws {Error} If the payload is not a reader or valid buffer
          * @throws {protobuf.util.ProtocolError} If required fields are missing
          */
-        public static decode(reader: (protobuf.Reader|Uint8Array), length?: number): msg.C2GW_EnableMaidDropTo;
+        public static decode(reader: (protobuf.Reader|Uint8Array), length?: number): msg.GW2C_EnableMaidDropTo;
 
         /**
-         * Decodes a C2GW_EnableMaidDropTo message from the specified reader or buffer, length delimited.
+         * Decodes a GW2C_EnableMaidDropTo message from the specified reader or buffer, length delimited.
          * @param reader Reader or buffer to decode from
-         * @returns C2GW_EnableMaidDropTo
+         * @returns GW2C_EnableMaidDropTo
          * @throws {Error} If the payload is not a reader or valid buffer
          * @throws {protobuf.util.ProtocolError} If required fields are missing
          */
-        public static decodeDelimited(reader: (protobuf.Reader|Uint8Array)): msg.C2GW_EnableMaidDropTo;
+        public static decodeDelimited(reader: (protobuf.Reader|Uint8Array)): msg.GW2C_EnableMaidDropTo;
 
         /**
-         * Verifies a C2GW_EnableMaidDropTo message.
+         * Verifies a GW2C_EnableMaidDropTo message.
          * @param message Plain object to verify
          * @returns `null` if valid, otherwise the reason why it is not
          */
         public static verify(message: { [k: string]: any }): (string|null);
 
         /**
-         * Creates a C2GW_EnableMaidDropTo message from a plain object. Also converts values to their respective internal types.
+         * Creates a GW2C_EnableMaidDropTo message from a plain object. Also converts values to their respective internal types.
          * @param object Plain object
-         * @returns C2GW_EnableMaidDropTo
+         * @returns GW2C_EnableMaidDropTo
          */
-        public static fromObject(object: { [k: string]: any }): msg.C2GW_EnableMaidDropTo;
+        public static fromObject(object: { [k: string]: any }): msg.GW2C_EnableMaidDropTo;
 
         /**
-         * Creates a plain object from a C2GW_EnableMaidDropTo message. Also converts values to other types if specified.
-         * @param message C2GW_EnableMaidDropTo
+         * Creates a plain object from a GW2C_EnableMaidDropTo message. Also converts values to other types if specified.
+         * @param message GW2C_EnableMaidDropTo
          * @param [options] Conversion options
          * @returns Plain object
          */
-        public static toObject(message: msg.C2GW_EnableMaidDropTo, options?: protobuf.IConversionOptions): { [k: string]: any };
+        public static toObject(message: msg.GW2C_EnableMaidDropTo, options?: protobuf.IConversionOptions): { [k: string]: any };
 
         /**
-         * Converts this C2GW_EnableMaidDropTo to JSON.
+         * Converts this GW2C_EnableMaidDropTo to JSON.
          * @returns JSON object
          */
         public toJSON(): { [k: string]: any };
@@ -17741,6 +17850,9 @@ declare namespace msg {
 
         /** C2GW_BuyTradeHouse tradeuid */
         tradeuid?: (number|Long|null);
+
+        /** C2GW_BuyTradeHouse houseuid */
+        houseuid?: (number|Long|null);
     }
 
     /** Represents a C2GW_BuyTradeHouse. */
@@ -17754,6 +17866,9 @@ declare namespace msg {
 
         /** C2GW_BuyTradeHouse tradeuid. */
         public tradeuid: (number|Long);
+
+        /** C2GW_BuyTradeHouse houseuid. */
+        public houseuid: (number|Long);
 
         /**
          * Creates a new C2GW_BuyTradeHouse instance using the specified properties.
@@ -18417,8 +18532,8 @@ declare namespace msg {
     /** Properties of a C2GW_CancelTradeHouse. */
     interface IC2GW_CancelTradeHouse {
 
-        /** C2GW_CancelTradeHouse tradeuid */
-        tradeuid?: (number|Long|null);
+        /** C2GW_CancelTradeHouse houseuid */
+        houseuid?: (number|Long|null);
     }
 
     /** Represents a C2GW_CancelTradeHouse. */
@@ -18430,8 +18545,8 @@ declare namespace msg {
          */
         constructor(properties?: msg.IC2GW_CancelTradeHouse);
 
-        /** C2GW_CancelTradeHouse tradeuid. */
-        public tradeuid: (number|Long);
+        /** C2GW_CancelTradeHouse houseuid. */
+        public houseuid: (number|Long);
 
         /**
          * Creates a new C2GW_CancelTradeHouse instance using the specified properties.
@@ -23482,6 +23597,9 @@ declare namespace table {
         /** TLevelMaidDefine ProduceTime */
         ProduceTime?: (number|Long|null);
 
+        /** TLevelMaidDefine ProduceItem */
+        ProduceItem?: (string[]|null);
+
         /** TLevelMaidDefine NextLevel */
         NextLevel?: (number|null);
     }
@@ -23509,6 +23627,9 @@ declare namespace table {
 
         /** TLevelMaidDefine ProduceTime. */
         public ProduceTime: (number|Long);
+
+        /** TLevelMaidDefine ProduceItem. */
+        public ProduceItem: string[];
 
         /** TLevelMaidDefine NextLevel. */
         public NextLevel: number;
