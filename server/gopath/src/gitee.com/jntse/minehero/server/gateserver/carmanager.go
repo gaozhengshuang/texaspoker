82a3ccb358e9c6d2ecc83ed7d918e4ec85354025
Franky <liushuaigeq@163.com>
2018-09-03 10:03:04 +0800

    fix

--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/carmanager.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/carmanager.go
@@ -856,7 +856,7 @@ func (this *CarManager) TakeBackFromParking(user *GateUser, parkingid uint64, op
 	//可以收回
 	parking.TakeBack()
 	if parking.IsPublic() {
-		privateParkings := this.GetParkingByUser(parking.date.GetOwnerid())
+		privateParkings := this.GetParkingByUser(parking.data.GetOwnerid())
 		takebacked := false
 		for _, v := range privateParkings {
 			if v.data.GetParkingcar() == car.data.GetId() {
