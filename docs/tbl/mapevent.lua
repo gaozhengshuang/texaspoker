e9e7e9b65a8785aad24bb03cfef9316b29fc47bb
biqiang <125356843@qq.com>
2018-09-04 18:42:01 +0800

    tbl

--- docs/tbl/mapevent.lua
+++ docs/tbl/mapevent.lua
@@ -3,13 +3,14 @@
 
 module table {
 export var TMapEvent : table.ITMapEventDefine[] = [
-		{ Id : 1001, Type : 1, Desc : "玩游戏得金币", MoneyType : 3, Price : 1, Icon : "", Reward : "" 	},
-		{ Id : 1002, Type : 1, Desc : "玩游戏得金币", MoneyType : 3, Price : 1, Icon : "", Reward : "" 	},
-		{ Id : 1003, Type : 1, Desc : "玩游戏得道具", MoneyType : 3, Price : 1, Icon : "", Reward : "" 	},
-		{ Id : 2001, Type : 2, Desc : "随机奖励金币", MoneyType : 3, Price : 1, Icon : "", Reward : "1-1;2-2" 	},
-		{ Id : 2002, Type : 2, Desc : "随机奖励体力", MoneyType : 3, Price : 1, Icon : "", Reward : "1-1;2-2" 	},
-		{ Id : 3001, Type : 3, Desc : "商店", MoneyType : 3, Price : 0, Icon : "", Reward : "" 	},
-		{ Id : 3002, Type : 3, Desc : "商店", MoneyType : 3, Price : 0, Icon : "", Reward : "" 	}
+		{ Id : 1001, Type : 1, Desc : "弹弹乐", MoneyType : 3, Price : 1, Icon : "eventsicon/3003.png", Reward : "" 	},
+		{ Id : 1002, Type : 1, Desc : "超市钩物", MoneyType : 3, Price : 1, Icon : "eventsicon/3003.png", Reward : "" 	},
+		{ Id : 1003, Type : 1, Desc : "红包翻翻乐", MoneyType : 3, Price : 1, Icon : "eventsicon/3003.png", Reward : "" 	},
+		{ Id : 2001, Type : 2, Desc : "随机奖励金币", MoneyType : 3, Price : 1, Icon : "eventsicon/3003.png", Reward : "1-1;2-2" 	},
+		{ Id : 2002, Type : 2, Desc : "随机奖励体力", MoneyType : 3, Price : 1, Icon : "eventsicon/3003.png", Reward : "1-1;2-2" 	},
+		{ Id : 3001, Type : 3, Desc : "女仆商店", MoneyType : 3, Price : 0, Icon : "eventsicon/3003.png", Reward : "" 	},
+		{ Id : 3002, Type : 3, Desc : "汽车配件商店", MoneyType : 3, Price : 0, Icon : "eventsicon/3003.png", Reward : "" 	},
+		{ Id : 3003, Type : 3, Desc : "房屋升级碎片商店", MoneyType : 3, Price : 0, Icon : "eventsicon/3003.png", Reward : "" 	}
 	]
 
 
