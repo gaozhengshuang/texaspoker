b71a2b5d76735404e698e9e8d7362499be2de546
jntse <xiejian1998@qq.com>
2018-09-04 16:11:08 +0800

    地图事件

--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/userevent.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/userevent.go
@@ -60,6 +60,43 @@ func (m *UserMapEvent) SendEvents() {
 func (m *UserMapEvent) Refresh() {
 	//x, y := m.owner.GetUserPos()		// 经纬度
 	//intx, inty := uint32(x * 1000) , uint32(y * 1000)
+
+	/*
+	ParseProString := func (sliceweight* []util.WeightOdds, Pro []string) (bool) {
+		for _ , strpro := range Pro {
+			slicepro := strings.Split(strpro, "-")
+			if len(slicepro) != 3 {
+				log.Error("[地图事件] 解析道具产出概率配置异常 strpro=%s", strpro)
+				return false
+			}
+			id    , _ := strconv.ParseInt(slicepro[0], 10, 32)
+			weight, _ := strconv.ParseInt(slicepro[1], 10, 32)
+			num,    _ := strconv.ParseInt(slicepro[2], 10, 32)
+			*sliceweight = append(*sliceweight, util.WeightOdds{Weight:int32(weight), Uid:int64(id), Num:int64(num)})
+		}
+		return true
+	}
+
+	giftweight := make([]util.WeightOdds, 0)
+	levelbase, ok := tbl.LevelMaidBase.TLevelMaidById[uint32(maid.Level())]
+	if ok == false {
+		log.Error("[女仆] 找不到女仆等级配置")
+		return
+	}
+
+	if ParseProString(&giftweight, levelbase.ProduceItem) == false { 
+		log.Error("[女仆] 解析道具产出配置失败")
+		return
+	}
+
+	index := util.SelectByWeightOdds(giftweight)
+	if index < 0 || index >= int32(len(giftweight)) {
+		log.Error("[女仆] 权重获得产出道具失败")
+		return
+	}
+
+	uid, num := giftweight[index].Uid, giftweight[index].Num
+	*/
 }
 
 
