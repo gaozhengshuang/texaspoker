8962419add99b8d4dd48484a975ab84acaed5adc
jntse <xiejian1998@qq.com>
2018-09-05 21:04:03 +0800

    地图事件

--- server/gopath/src/gitee.com/jntse/minehero/server/gateserver/mapstore.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/gateserver/mapstore.go
@@ -63,12 +63,12 @@ func (cp *StoreProduct) SaveBin(pipe redis.Pipeliner) {
 	pid := strconv.FormatUint(uint64(cp.Pid()), 10)
 	cp.dirty = false
 	if pipe == nil {
-		if err := utredis.HSetProtoBin(Redis(), "carshop", pid, cp.bin); err != nil {
-			log.Error("[汽车商店] 保存产品数据 Redis Error:%s", err)
+		if err := utredis.HSetProtoBin(Redis(), "mapstore", pid, cp.bin); err != nil {
+			log.Error("[地图商店] 保存产品数据 Redis Error:%s", err)
 		}
-		log.Error("[汽车商店] 保存产品数据成功 pid[%d]", pid)
+		log.Error("[地图商店] 保存产品数据成功 pid[%d]", pid)
 	}else {
-		utredis.HSetProtoBinPipeline(pipe, "carshop", pid, cp.bin)
+		utredis.HSetProtoBinPipeline(pipe, "mapstore", pid, cp.bin)
 	}
 }
 
@@ -92,73 +92,69 @@ type MapStore struct {
 	ticker1Minite 	*util.GameTicker
 }
 
-func (shop* MapStore) Init() {
-	shop.products = make(map[uint32] *StoreProduct)
-	shop.ticker1Minite = util.NewGameTicker(time.Minute, shop.Handler1MiniteTick)
-	shop.ticker1Minite.Start()
-
-	shop.LoadDB()
-	//shop.InitOnce()
+func (store* MapStore) Init() {
+	store.products = make(map[uint32] *StoreProduct)
+	store.ticker1Minite = util.NewGameTicker(time.Minute, store.Handler1MiniteTick)
+	store.ticker1Minite.Start()
+	store.LoadDB()
 }
 
-func (shop* MapStore) Tick(now int64) {
-	shop.ticker1Minite.Run(now)
+func (store* MapStore) Tick(now int64) {
+	store.ticker1Minite.Run(now)
 }
 
-func (shop *MapStore) Handler1MiniteTick(now int64) {
+func (store *MapStore) Handler1MiniteTick(now int64) {
 	pipe := Redis().Pipeline()
-	for _, v := range shop.products {
+	for _, v := range store.products {
 		if v.Dirty() == true { v.SaveBin(pipe) }
 	}
 	_, err := pipe.Exec()
 	if err != nil && err != redis.Nil {
-		log.Error("[汽车商店] 定时存盘RedisError:%s", err)
+		log.Error("[地图商店] 定时存盘RedisError:%s", err)
 	}
 	pipe.Close()
 }
 
-// 第一次初始化
-//func (shop *MapStore) InitOnce() {
-//	if len(shop.products) != 0 {
-//		return
-//	}
-//	for _, v := range tbl.MapStoreBase.TMapStoreById {
-//		shop.products[v.Id] = NewStoreProduct(v)
-//	}
-//	log.Info("[汽车商店] 首次初始化")
-//	shop.SaveAll()
-//}
+func (store *MapStore) Refresh() {
+	store.products = make(map[uint32] *StoreProduct)
+	for _, v := range tbl.MapStoreBase.TMapStoreById {
+		store.products[v.Id] = NewStoreProduct(v)
+	}
+	log.Info("[地图商店] 刷新所有商店库存 总数[%d]", len(store.products))
+	store.SaveAll()
+}
 
-//func (shop *MapStore) PutNewCar(pid, num uint32) {
-//	config, find := tbl.MapStoreBase.TMapStoreById[pid]
-//	if find == false {
-//		log.Error("[汽车商店] 投放新的车辆失败，无效的商品id[%d]", pid)
-//		return
-//	}
-//
-//	product, ok := shop.products[pid]
-//	if ok == true {
-//		product.SetSell(product.Sell() + num)
-//	}else {
-//		product = NewStoreProduct(config)
-//		product.SetSell(num)
-//		shop.products[pid] = product
-//	}
-//
-//	log.Info("[汽车商店] 投放新的车辆成功，商品id[%d] 数量[%d]", pid, num)
-//}
+// 投放新库存
+func (store *MapStore) PutNewProduct(pid, num uint32) {
+	config, find := tbl.MapStoreBase.TMapStoreById[pid]
+	if find == false {
+		log.Error("[地图商店] 投放新的商品失败，无效的商品id[%d]", pid)
+		return
+	}
+
+	product, ok := store.products[pid]
+	if ok == true {
+		product.SetSell(product.Sell() + num)
+	}else {
+		product = NewStoreProduct(config)
+		product.SetSell(num)
+		store.products[pid] = product
+	}
+
+	log.Info("[地图商店] 投放新的商品成功，商品id[%d] 数量[%d]", pid, num)
+}
 
 // DB加载
-func (shop* MapStore) LoadDB() {
+func (store* MapStore) LoadDB() {
 	pipe := Redis().Pipeline()
 	defer pipe.Close()
 	for _, v := range tbl.MapStoreBase.TMapStoreById {
 		pid := strconv.FormatUint(uint64(v.Id), 10)
-		pipe.HGet("carshop", pid)
+		pipe.HGet("mapstore", pid)
 	}
 	cmds, err := pipe.Exec()
 	if err != nil && err != redis.Nil {
-		log.Error("[汽车商店] MapStore LoadDB RedisError:%s ", err)
+		log.Error("[地图商店] MapStore LoadDB RedisError:%s ", err)
 		return
 	}
 
@@ -167,103 +163,112 @@ func (shop* MapStore) LoadDB() {
 		rbuf, _ := v.(*redis.StringCmd).Bytes()
 		product := &StoreProduct{}
 		if product.LoadBin(rbuf) != nil {
-			shop.products[product.Pid()] = product
+			store.products[product.Pid()] = product
 		}
 	}
-	log.Info("[汽车商店] DB加载数据 size=%d", len(shop.products))
+	log.Info("[地图商店] DB加载数据 size=%d", len(store.products))
 }
 
 // 保存
-func (shop *MapStore) SaveAll() {
+func (store *MapStore) SaveAll() {
 	pipe := Redis().Pipeline()
 	defer pipe.Close()
-	for _, v := range shop.products {
+	for _, v := range store.products {
 		v.SaveBin(pipe)
 	}
 	_, err := pipe.Exec()
 	if err != nil {
-		log.Error("[汽车商店] MapStore SaveAll Error:%s", err)
+		log.Error("[地图商店] MapStore SaveAll Error:%s", err)
 		return
 	}
-	log.Info("[汽车商店] 保存所有数据")
+	log.Info("[地图商店] 保存所有数据")
 }
 
 // 购买汽车
-func (shop *MapStore) BuyCar(user *GateUser, shopid, pid uint32) {
+func (store *MapStore) BuyProduct(user *GateUser, shopid, pid, num uint32) {
 	config, ok := tbl.MapStoreBase.TMapStoreById[pid]
 	if ok == false {
-		user.SendNotify("购买的车辆不存在")
-		log.Error("[汽车商店] 玩家[%s %d] 购买车辆不在出售列表[%d]", user.Name(), user.Id(), pid)
+		user.SendNotify("购买的商品不存在")
+		log.Error("[地图商店] 玩家[%s %d] 购买商品不在出售列表[%d]", user.Name(), user.Id(), pid)
 		return
 	}
 
-	cartemplate, ok := tbl.TCarBase.TCarById[config.Carid]
+	itembase, ok := tbl.ItemBase.ItemBaseDataById[config.Id]
 	if ok == false {
-		log.Error("[汽车商店] 玩家[%s %d] 购买车辆但车辆配置无效[%d]", user.Name(), user.Id(), config.Carid)
+		log.Error("[地图商店] 玩家[%s %d] 购买商品但商品配置无效[%d]", user.Name(), user.Id(), config.Id)
 		return
 	}
 
-	if config.Price > user.GetGold() {
-		user.SendNotify("金币不足")
-		return
-	}
-
-	product, ok := shop.products[pid]
+	product, ok := store.products[pid]
 	if ok == false {
 		product = NewStoreProduct(config)
 		product.SaveBin(nil)
 	}
 
 	// 限量
-	if product.Sell() <= product.Sold() {
-		user.SendNotify("车辆数量不足")
+	if product.Sell() < product.Sold() + num {
+		user.SendNotify("商品数量不足")
 		return
 	}
 
+	// 价格检查
+	if config.MoneyType == uint32(msg.MoneyType__Gold) {
+		if config.Price > user.GetGold() {
+			user.SendNotify("金币不足")
+			return
+		}
+		user.RemoveGold(config.Price, "地图商店购买", true)
+	}else if config.MoneyType == uint32(msg.MoneyType__Diamond) {
+		if config.Price > user.GetDiamond() {
+			user.SendNotify("钻石不足")
+			return
+		}
+		user.RemoveDiamond(config.Price, "地图商店购买", true)
+	}else {
+		log.Error("[地图商店] 玩家[%s %d] 购买商品但商品价格类型无效%d", user.Name(), user.Id(), config.MoneyType)
+		return
+	}
 
-	user.RemoveGold(config.Price, "购买车辆", true)
 	product.AddSold(1)
-	cardata := CarMgr().CreateNewCar(user.Id(), cartemplate.Id, user.Name(),config.Price)
-
-	carmsg := &msg.GW2C_AddNewCar{}
-	carmsg.Car = pb.Clone(cardata.PackBin()).(*msg.CarData)
-	user.SendMsg(carmsg)
+	user.AddItem(itembase.Id, num, "地图商店购买", true)
 
 	// 刷新单条数据
 	send := &msg.GW2C_UpdateMapStoreProduct{Product:pb.Clone(product.Bin()).(*msg.StoreProductData)}
 	user.SendMsg(send)
 }
 
-// 发送汽车商店信息
-//func (shop *MapStore) SendShopInfo(user *GateUser) {
-//	send := &msg.GW2C_SendMapStoreInfo{}
-//	for _, v := range shop.products {
-//		send.Products = append(send.Products, pb.Clone(v.Bin()).(*msg.StoreProductData))
-//	}
-//	user.SendMsg(send)
-//}
+// 发送地图商店信息
+func (store *MapStore) SendStoreInfo(user *GateUser, tid uint32) {
+	send := &msg.GW2C_SendMapStoreInfo{Tid:pb.Uint32(tid)}
+	for _, v := range store.products {
+		send.Products = append(send.Products, pb.Clone(v.Bin()).(*msg.StoreProductData))
+	}
+	user.SendMsg(send)
+}
 
-//func (shop *MapStore) DoGMCmd(cmd map[string]*util.VarType) {
-//	opt, ok := cmd["opt"]
-//	if !ok || opt == nil {
-//		return
-//	}
-//
-//	id, ok := cmd["id"]
-//	if !ok || id == nil {
-//		return
-//	}
-//
-//	num, ok := cmd["num"]
-//	if !ok || num == nil {
-//		return
-//	}
-//
-//	switch opt.String() {
-//	case "inc":
-//		shop.PutNewCar(id.Uint32(), num.Uint32())
-//		break
-//	case "dec":
-//		break
-//	}
-//}
+func (store *MapStore) DoGMCmd(cmd map[string]*util.VarType) {
+	opt, ok := cmd["opt"]
+	if !ok || opt == nil {
+		return
+	}
+
+	id, ok := cmd["id"]
+	if !ok || id == nil {
+		return
+	}
+
+	num, ok := cmd["num"]
+	if !ok || num == nil {
+		return
+	}
+
+	switch opt.String() {
+	case "inc":
+		store.PutNewProduct(id.Uint32(), num.Uint32())
+		break
+	case "dec":
+		break
+	}
+}
+
+
