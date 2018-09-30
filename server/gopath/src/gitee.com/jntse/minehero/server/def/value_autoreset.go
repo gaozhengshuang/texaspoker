efd9dc67487e2dc51a071305547c7dab44a759b5
jntse <xiejian1998@qq.com>
2018-09-30 15:12:12 +0800

    自动重置变量封装

--- server/gopath/src/gitee.com/jntse/minehero/server/def/value_autoreset.go
+++ server/gopath/src/gitee.com/jntse/minehero/server/def/value_autoreset.go
@@ -7,7 +7,7 @@ import (
 )
 
 // 每日整点h重置变量
-type InthourAutoResetValue struct {
+type AutoResetValue struct {
 	id			int32		//
 	hours		int32		// hours
 	value 		int64		// value
@@ -15,14 +15,14 @@ type InthourAutoResetValue struct {
 	nextreset	int64		// tmp timestamp
 }
 
-func NewInthourAutoResetValue(id, hours int32, value int64) *InthourAutoResetValue {
-	v := &InthourAutoResetValue{id:id, hours:hours}
-	v.Init()
-	v.Inc(value)
+func NewAutoResetValue(id, hours int32, value int64) *AutoResetValue {
+	v := &AutoResetValue{id:id, hours:hours}
+	v.init()
+	v.inc(value)
 	return v
 }
 
-func (t *InthourAutoResetValue) init() {
+func (t *AutoResetValue) init() {
 	resetbaseline, now := util.GetDayStart() + int64(t.hours * util.HourSec), util.CURTIME()
 	if t.lastreset == 0 {	// firt init
 		t.value ,t.lastreset = 0, now
@@ -37,17 +37,17 @@ func (t *InthourAutoResetValue) init() {
 	}
 }
 
-func (t *InthourAutoResetValue) loadBin(bin *msg.InthourAutoResetValue) {
+func (t *AutoResetValue) loadBin(bin *msg.AutoResetValue) {
 	t.id, t.hours, t.value = bin.GetId(), bin.GetHours(), bin.GetValue()
-	t.Init()
+	t.init()
 }
 
-func (t *InthourAutoResetValue) packBin() *msg.InthourAutoResetValue {
-	bin := &msg.InthourAutoResetValue{Id:pb.Int32(t.id), Hours:pb.Int32(t.hours), Value:pb.Int64(t.value)}
+func (t *AutoResetValue) packBin() *msg.AutoResetValue {
+	bin := &msg.AutoResetValue{Id:pb.Int32(t.id), Hours:pb.Int32(t.hours), Value:pb.Int64(t.value)}
 	return bin
 }
 
-func (t *InthourAutoResetValue) check() {
+func (t *AutoResetValue) check() {
 	now := util.CURTIME()
 	if now > t.nextreset {
 		t.value = 0
@@ -56,85 +56,85 @@ func (t *InthourAutoResetValue) check() {
 	}
 }
 
-func (t *InthourAutoResetValue) value() int64 {
-	t.Check()
+func (t *AutoResetValue) val() int64 {
+	t.check()
 	return t.value
 }
 
-func (t *InthourAutoResetValue) inc(n int64) {
-	t.Check()
+func (t *AutoResetValue) inc(n int64) {
+	t.check()
 	t.value += n
 }
 
-func (t *InthourAutoResetValue) dec(n int64) {
-	t.Check()
+func (t *AutoResetValue) dec(n int64) {
+	t.check()
 	t.value -= n
 }
 
 // 自动重置变量管理器
-type InthourAutoResetManager struct {
-	values map[int32]*InthourAutoResetValue
+type AutoResetValueManager struct {
+	values map[int32]*AutoResetValue
 }
 
 // 加载
-func (m *InthourAutoResetManager) LoadBin(bin *msg.InthourAutoResetManager) {
+func (m *AutoResetValueManager) LoadBin(bin *msg.AutoResetValueManager) {
 	for _, v := range bin.Values {
-		vv := &InthourAutoResetValue{}
-		vv.LoadBin(v)
+		vv := &AutoResetValue{}
+		vv.loadBin(v)
 		m.values[vv.id] = vv
 	}
 }
 
 // 打包
-func (m *InthourAutoResetManager) PackBin() *msg.InthourAutoResetManager {
-	bin := &msg.InthourAutoResetManager{Values:make([]*msg.InthourAutoResetValue, 0)}
+func (m *AutoResetValueManager) PackBin() *msg.AutoResetValueManager {
+	bin := &msg.AutoResetValueManager{Values:make([]*msg.AutoResetValue, 0)}
 	for _, v := range m.values {
-		bin.Values = append(bin.Values, v.PackBin())
+		bin.Values = append(bin.Values, v.packBin())
 	}
 	return bin
 }
 
 // 获取值
-func (m *InthourAutoResetManager) Val(id int32) (int64, error) {
+func (m *AutoResetValueManager) Val(id int32) (int64, error) {
 	v, ok := m.values[id]
 	if ok == false {
 		return 0, fmt.Errorf("value is not exist")
 	}
-	return v.Value(), nil
+	return v.val(), nil
 }
 
 // 增加
-func (m *InthourAutoResetManager) Inc(id int32, n int64) error {
+func (m *AutoResetValueManager) Inc(id int32, n int64) error {
 	v, ok := m.values[id]
 	if ok == false {
 		return fmt.Errorf("value is not exist")
 	}
-	v.Inc(n)
+	v.inc(n)
 	return nil
 }
 
 // 减少
-func (m *InthourAutoResetManager) Dec(id int32, n int64) error {
+func (m *AutoResetValueManager) Dec(id int32, n int64) error {
 	v, ok := m.values[id]
 	if ok == false {
 		return fmt.Errorf("value is not exist")
 	}
-	v.Dec(n)
+	v.dec(n)
 	return nil
 }
 
 // 添加新计数器
-func (m *InthourAutoResetManager) Add(id, hours int32, n int64) error {
+func (m *AutoResetValueManager) Add(id, hours int32, n int64) error {
 	_, ok := m.values[id]
 	if ok == true {
 		return fmt.Errorf("value is duplicated")
 	}
-	m.values[id] = NewInthourAutoResetValue(id, hours, n)
+	m.values[id] = NewAutoResetValue(id, hours, n)
 	return nil
 }
 
 // 删除计数器
-func (m *InthourAutoResetManager) Remove(id int32) {
+func (m *AutoResetValueManager) Remove(id int32) {
 	delete(m.values, id)
 }
 
