fd88d56547f98294f078ae3dbe1ba302de12843b
jntse <xiejian1998@qq.com>
2018-10-31 11:51:01 +0800

    添加mysql支持

---
+++ server/gopath/src/gitee.com/jntse/minehero/server/sql/init.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS texaspoker;
+CREATE DATABASE texaspoker;
+
+use texaspoker;
+
+DROP TABLE IF EXISTS `charbase`;
+CREATE TABLE `charbase` (
+    `id` bigint(20) NOT NULL,
+    `name` varchar(32) NOT NULL DEFAULT "",
+    `head` int(10) NOT NULL DEFAULT '0',
+    `sex` int(10) NOT NULL DEFAULT '0',
+    `account` varchar(32) NOT NULL DEFAULT '0',
+    `level` int(10) NOT NULL DEFAULT '0',
+    `exp` int(10) NOT NULL DEFAULT '0',
+    `gold` bigint(20) NOT NULL DEFAULT '0',
+    `yuanbao` bigint(20) NOT NULL DEFAULT '0',
+    `diamond` bigint(20) NOT NULL DEFAULT '0',
+    `age` int(10) NOT NULL DEFAULT '0',
+    `vipexp` int(10) NOT NULL DEFAULT '0',
+    `viplevel` int(10) NOT NULL DEFAULT '0',
+    `viptime1` bigint(20) NOT NULL DEFAULT '0',
+    `viptime2` bigint(20) NOT NULL DEFAULT '0',
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+
