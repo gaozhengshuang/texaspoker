c1550891a7835a34e953caaab9c7350db741ab23
zhangmaodong <527129180@qq.com>
2018-11-01 15:03:53 +0800

    任务代码还原

--- client/src/app/achievement/AchievementManager.ts
+++ client/src/app/achievement/AchievementManager.ts
@@ -131,7 +131,6 @@ class AchievementManager
                 if (allInfo.definition.Group == ainfo.groupid)
                 {
                     allInfo.isActive = true;
-                    break;
                 }
             }
         }
