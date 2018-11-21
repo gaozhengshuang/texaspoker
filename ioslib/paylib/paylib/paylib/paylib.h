659bb19cb14f85278e57c3318a67688a75a746ca
ztadmin <330412748@qq.com>
2018-11-21 10:28:41 +0800

    new ioslib

---
+++ ioslib/paylib/paylib/paylib/paylib.h
@@ -0,0 +1,19 @@
+//
+//  paylib.h
+//  paylib
+//
+//  Created by ztadmin on 2018/11/20.
+//  Copyright © 2018年 ztadmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for paylib.
+FOUNDATION_EXPORT double paylibVersionNumber;
+
+//! Project version string for paylib.
+FOUNDATION_EXPORT const unsigned char paylibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <paylib/PublicHeader.h>
+
+
